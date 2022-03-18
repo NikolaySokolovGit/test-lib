e8f249fd824c7e92e90e3a8073a9ba0177c44d39
Nikolay Sokolov <60136678+NikolaySokolovGit@users.noreply.github.com>
2022-03-18 19:04:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
-    url="https://github.com/pypa/samplepro,
+    url="https://github.com/pypa/samplepro",
     package_dir={"main": "main"},
     python_requires=">=3.6",
 )
