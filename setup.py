01e90b17b19a2b84c1a64f878f2a0395fc1fb587
Nikolay Sokolov <60136678+NikolaySokolovGit@users.noreply.github.com>
2022-03-18 19:03:54 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setuptools.setup(
+    name="test-value",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/samplepro,
+    package_dir={"main": "main"},
+    python_requires=">=3.6",
+)
